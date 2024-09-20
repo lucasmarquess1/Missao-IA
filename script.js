9f445188352c5e2e84f2fafe72efc794b4815931
lucasmarquess1 <130072204+lucasmarquess1@users.noreply.github.com>
2024-09-19 21:37:23 -0300

    teste

--- script.js
+++ script.js
@@ -10,22 +10,41 @@ const perguntas = [
         alternativas: [
             {
             texto:"Isso é assustador!",
-            afirmacao:"Isso é maravilhoso!"
+            afirmacao:"Afirmação"
+            },
+        
+            {
+            texto:"Isso é maravilhoso!",
+            afirmacao:"Afirmação"
             }
         ]
     },
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            {
+            texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            afirmacao:"Afirmação"
+            },
+
+            {
+            texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            afirmacao:"Afirmação"
+            }
         ]
     },
     {
         enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            {
+            texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            afirmacao:"Afirmação"
+            },
+
+            {
+            texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+            afirmacao:"Afirmação"
+            }
         ]
     },
     {
@@ -55,7 +74,7 @@ function mostraPergunta() {
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        cont botaoAltrnativas = document.createElement("button");
+        const botaoAltrnativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", function(){
           atual++;
